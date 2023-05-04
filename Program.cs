b47e71342926c51b24722528eb663ffbcbec0431
Pawan72603 <132444564+Pawan72603@users.noreply.github.com>
2023-05-04 20:43:59 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,31 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp3
+namespace ConsoleApplication2
 {
-    class Program
+ class Program
     {
-        
+     static void Palindrome()
+     {
+         string name = "", reversestring = "";                                                //create two variables 
+
+         Console.WriteLine("Enter the String");                                              // for instrcution to enter the string 
+         name = Console.ReadLine();                                                         // input string through keyboard 
+         for (int i = name.Length - 1; i >= 0; i--)                                        //created for loop to reverse the string 
+         {
+             reversestring += name[i];
+         }
+
+         if (reversestring == name)                                                      //if condition for checking the name string is equal or not 
+         {
+             Console.WriteLine(name + "  is a palindrome Value and the reverse value is" + reversestring);
+         }
+         else
+         {
+             Console.WriteLine(name + " is not palindrome Value and the reverse value is :" + reversestring);
+         }
+         Console.ReadLine();                                                            //to shows the result on Consol screen
+     }
         static void Main(string[] args)
         {
-            string name = "", reversestring = "";         //create two variables 
-
-            Console.WriteLine("Enter the String");       // for instrcution to enter the string 
-            name = Console.ReadLine();                   // input string through keyboard 
-            for (int i = name.Length-1; i>=0; i--)       //created for loop to reverse the string 
-            {
-                reversestring += name[i];
-            }
-
-            if (reversestring == name)                                                      //if condition for checking the name string is equal or not 
-            {                                                                                
-                Console.WriteLine($"The String is palindrome:{reversestring}");
-            }
-            else
-            {
-                Console.WriteLine($"The String is not Palindrome:{reversestring}"); 
-            }
-            Console.ReadLine();                                     
+            Program.Palindrome();                                                       //call static method using Classname.
            
         }
     }
