e485228d9d86326b3e69861ff1667657ee16b9f8
Pawan72603 <132444564+Pawan72603@users.noreply.github.com>
2023-05-04 15:16:20 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string name = "", reversestring = "";         //create two variables 
+
+            Console.WriteLine("Enter the String");       // for instrcution to enter the string 
+            name = Console.ReadLine();                   // input string through keyboard 
+            for (int i = name.Length-1; i>=0; i--)       //created for loop to reverse the string 
+            {
+                reversestring += name[i];
+            }
+
+            if (reversestring == name)                                                      //if condition for checking the name string is equal or not 
+            {                                                                                
+                Console.WriteLine($"The String is palindrome:{reversestring}");
+            }
+            else
+            {
+                Console.WriteLine($"The String is not Palindrome:{reversestring}"); 
+            }
+            Console.ReadLine();                                     
+           
+        }
+    }
+}
