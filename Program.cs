84bd00ad1e8b0bd51d2be07304484c4fccd00fec
Pawan72603 <132444564+Pawan72603@users.noreply.github.com>
2023-05-05 09:23:19 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,35 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace StaticReadOnly
 {
- class Program
+    class Program
     {
-     static void Palindrome()
-     {
-         string name = "", reversestring = "";                                                //create two variables 
-
-         Console.WriteLine("Enter the String");                                              // for instrcution to enter the string 
-         name = Console.ReadLine();                                                         // input string through keyboard 
-         for (int i = name.Length - 1; i >= 0; i--)                                        //created for loop to reverse the string 
-         {
-             reversestring += name[i];
-         }
-
-         if (reversestring == name)                                                      //if condition for checking the name string is equal or not 
-         {
-             Console.WriteLine(name + "  is a palindrome Value and the reverse value is" + reversestring);
-         }
-         else
-         {
-             Console.WriteLine(name + " is not palindrome Value and the reverse value is :" + reversestring);
-         }
-         Console.ReadLine();                                                            //to shows the result on Consol screen
-     }
-        static void Main(string[] args)
+        static Program()
         {
-            Program.Palindrome();                                                       //call static method using Classname.
-           
+            designation = "Trainee";
+        }
+        static readonly int Id=84;
+        static readonly string name = "Pawan Kumar Chaurasiya";
+        static readonly string designation = "";
+        static void Main()
+        {
+            Program obj = new Program();
+            Console.WriteLine(Program.name + "designation is " + Program.designation + " holding id is " + Program.Id);
+            Console.ReadLine();
+
         }
     }
 }
