ea8ab084748b739e1244ae712ef428d936737bb7
Pawan72603 <132444564+Pawan72603@users.noreply.github.com>
2023-05-05 12:33:11 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Class1
+    {
+        
+
+        static void Main()
+        {
+            String Name = Console.ReadLine();
+            int n = 1;
+            for (int i = 0; i <Name.Length; i++)
+            {
+                for (int j = i+1; j <Name.Length ; j++)
+                {
+                    if (Name[i] == Name[j])
+                    {
+                        Console.WriteLine(Name[i]);
+                        n++;
+    ;                    }
+                }
+            }Console.WriteLine(n+"");
+            
+        }Console.ReadLine();
+
+
+    }
+}
