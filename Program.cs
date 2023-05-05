57b87bf9c039b23402991d7d3be1bafeabb9b721
Pawan72603 <132444564+Pawan72603@users.noreply.github.com>
2023-05-05 12:29:51 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,23 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StaticReadOnly
+namespace ConsoleApp3
 {
     class Program
     {
-        static Program()
+        
+        static void Main(string[] args)
         {
-            designation = "Trainee";
-        }
-        static readonly int Id=84;
-        static readonly string name = "Pawan Kumar Chaurasiya";
-        static readonly string designation = "";
-        static void Main()
-        {
-            Program obj = new Program();
-            Console.WriteLine(Program.name + "designation is " + Program.designation + " holding id is " + Program.Id);
-            Console.ReadLine();
+            string name = "", reversestring = "";         //create two variables 
 
+            Console.WriteLine("Enter the String");       // for instrcution to enter the string 
+            name = Console.ReadLine();                   // input string through keyboard 
+            for (int i = name.Length-1; i>=0; i--)       //created for loop to reverse the string 
+            {
+                reversestring += name[i];
+            }
+
+            if (reversestring == name)                                                      //if condition for checking the name string is equal or not 
+            {                                                                                
+                Console.WriteLine($"The String is palindrome:{reversestring}");
+            }
+            else
+            {
+                Console.WriteLine($"The String is not Palindrome:{reversestring}"); 
+            }
+            Console.ReadLine();                                     
+           
         }
     }
 }
